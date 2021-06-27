90d6517d1f86f78575f0c87177008d3c8f07ec27
BogdanBtnl <86569442+BogdanBtnl@users.noreply.github.com>
2021-06-27 22:51:02 +0300

    Update bfc.py

--- bfc.py
+++ bfc.py
@@ -186,15 +186,6 @@ def search(x,y):
         if grid[int((288-y)/(24))][int((588+x)/24)] == "e":
             break
 
-
-def backRoute(x, y):
-    yellow.goto(x, y)
-    yellow.stamp()
-    while (x, y) != (start_x, start_y):
-        yellow.goto(solution[x, y])
-        yellow.stamp()
-        x, y = solution[x, y]
-
 maze = Maze()
 red = Red()
 blue = Blue()
@@ -209,5 +200,4 @@ solution = {}
 
 setup_maze(grid)
 search(start_x,start_y)
-# backRoute(end_x, end_y)
 wn.exitonclick()
